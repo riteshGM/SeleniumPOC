9d1d4359eb1a355959f92949eb865a655e65291c
Ritesh Mansukhani <riteshmansukhani@gmail.com>
2022-05-08 23:54:40 +0530

    Added Core Java Code

--- src/test/java/com/rnd/testscripts/NewTests.java
+++ src/test/java/com/rnd/testscripts/NewTests.java
@@ -1,31 +1,36 @@
 package com.rnd.testscripts;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import static com.learning.constants.FrameworkConstants.ASSERTION_FOR;
+
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.AfterClass;
 
-public class NewTests {
-	public WebDriver driver;
-	
-	  @Test
-	  public void openMyBlog() {
-		driver.get("https://www.softwaretestingmaterial.com/");
-	  }
-	  
-	  @BeforeClass
-	  public void beforeClass() {
-		  
-		  //System.setProperty("webdriver.gecko.driver", "D:\\Selenium\\Drivers\\geckodriver.exe");
-		  driver = new FirefoxDriver();
-		  
-	  }
+import com.learning.annotations.FrameworkAnnotation;
+import com.learning.enums.AuthorType;
+import com.learning.enums.CategoryType;
+import com.learning.pages.HomePage;
+import com.learning.pages.LoginPage;
+import com.learning.testdata.TestData;
+import com.learning.utils.DataProviderUtils;
+import com.learning.utils.VerificationUtils;
 
-	  @AfterClass
-	  public void afterClass() {
-		  driver.quit();
-	  }
+public class NewTests extends BaseTest {
+    @FrameworkAnnotation(author = {AuthorType.RAJAT, AuthorType.NISHANT},
+            category = {CategoryType.BVT, CategoryType.REGRESSION})
+    @Test(groups = {"BVT","REGRESSION"},description = "To check whether the User can not login with Invalid Credentials",
+            dataProvider = "getTestData", dataProviderClass = DataProviderUtils.class)
+    public void errorInvalidCredentials(TestData testData) {
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.loginToApplication(testData.getUsername(), testData.getPassword());
+        String errorInvalidCreds =  loginPage.getErrorMessage();
+
+//        Assert.assertEquals(errorInvalidCreds, testData.getExpectedError(),
+//                "Assertion for Invalid credentials Error message");
+
+        VerificationUtils.validateResponse(errorInvalidCreds,testData.getExpectedError(),
+                ASSERTION_FOR +" -  <b> <u> Invalid credentials Error message </b> </u>");
+
+    }
 
 
 }
