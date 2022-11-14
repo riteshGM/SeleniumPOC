0745fa8f05e9e8f3f9ca69255975c45b4bee0a19
Ritesh Mansukhani <riteshmansukhani@gmail.com>
2022-11-15 01:46:10 +0530

    Added Java Program

---
+++ src/test/java/com/rnd/testscripts/JavaProgram.java
@@ -0,0 +1,24 @@
+package com.rnd.testscripts;
+
+public class JavaProgram {
+	
+	public static void main (String args[]) {
+		System.out.println("Execution Started");
+		int n = 841;
+		int x = 1;
+		for (int i =1 ; i<50; i++) {
+			int nx = (x+n/x)/2;
+			x = nx;
+		}
+		System.out.println("Value of x = "+x);
+		System.out.println("Execution Ended");
+		
+
+			System.out.println(3+8*2-9-2);
+			
+			
+		
+		
+	}
+
+}
